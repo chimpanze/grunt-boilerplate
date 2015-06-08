a626b3385c0361b1f6154715e650f78125d12ccb
Marten Gülink <mail@marten.me>
2015-06-08 17:51:04 +0200

    Adding usemin to the boilerplate

--- gruntfile.js
+++ gruntfile.js
@@ -1,149 +1,203 @@
 var mozjpeg = require('imagemin-mozjpeg');
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  'use strict';
+    'use strict';
 
-  require('time-grunt')(grunt);
+    require('time-grunt')(grunt);
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-    connect: {
-      server: {
-        options: {
-          port: 1337,
-        }
-      }
-    },
-
-    watch: {
-      css: {
-        files: 'assets/dev/scss/**/*.scss',
-        tasks: ['sass:dev'],
-        options: {
-        atBegin: true,
-        interrupt: true,
-        spawn: true,
-        livereload: true
-        }
-      }
-    },
-
-    sass: {
-      dev: {
-        options: {
-          sourceMap: true,
-          outputStyle: 'nested'
+        connect: {
+            server: {
+                options: {
+                    port: 1337
+                }
+            }
         },
-        files: {
-          'assets/dev/css/style.css' : 'assets/dev/scss/style.scss'
-        }
-      },
-      prod: {
-        options: {
-          sourceMap: false,
-          outputStyle: 'compressed'
+
+        watch: {
+            css: {
+                files: 'assets/scss/**/*.scss',
+                tasks: ['sass:dev'],
+                options: {
+                    atBegin: true,
+                    interrupt: true,
+                    spawn: true,
+                    livereload: true
+                }
+            }
         },
-        files: {
-          'assets/prod/css/style.css' : 'assets/dev/scss/style.scss'
-        }
-      }
-    },
 
-    uglify: {
-      my_targets: {
-        files: {
-          'assets/prod/js/minified.js': ['assets/dev/js/*.js']
-        }
-      }
-    },
-
-    phantomcss: {
-      options: { mismatchTolerance: 0.05 },
-
-      viewportSmall: {
-        options: {
-          screenshots: 'css-regression-tests/viewportSmall/baselines',
-          results: 'css-regression-tests/viewportSmall/results',
-          viewportSize: [375, 667]
+        sass: {
+            dev: {
+                options: {
+                    sourceMap: true,
+                    outputStyle: 'nested'
+                },
+                files: {
+                    'assets/dev/css/style.css': 'assets/scss/style.scss'
+                }
+            }
         },
-        src: [ 'css-regression-tests/phantomcss.js' ]
-      },
-      // viewportMiddle: {},
-      // viewportLarge: {}
-    },
 
-    imagemin: {
-      options: {
-        optimizationLevel: 3,
-        progressive: true,
-        interlaced: true,
-        use: [mozjpeg()]
-      },
-      png: {
-        files: [{
-          expand: true,
-          cwd: 'assets/dev/',
-          src: ['images/png/**/*.png'],
-          dest: 'assets/prod/'
-        }]
-      },
-      jpg: {
-        files: [{
-          expand: true,
-          cwd: 'assets/dev/',
-          src: ['images/jpg/**/*.jpg'],
-          dest: 'assets/prod/',
-        }]
-      },
-      gif: {
-        files: [{
-          expand: true,
-          cwd: 'assets/dev/',
-          src: ['images/gif/**/*.gif'],
-          dest: 'assets/prod/'
-        }]
-      }
-    }
-  });
+        phantomcss: {
+            options: {mismatchTolerance: 0.05},
 
-  //TODO: usemin, image optimization,
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-phantomcss');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
+            viewportSmall: {
+                options: {
+                    screenshots: 'css-regression-tests/viewportSmall/baselines',
+                    results: 'css-regression-tests/viewportSmall/results',
+                    viewportSize: [375, 667]
+                },
+                src: ['css-regression-tests/phantomcss.js']
+            },
+            // viewportMiddle: {},
+            // viewportLarge: {}
+        },
 
-  grunt.registerTask('default',[
-    'connect',
-    'watch'
-  ]);
+        imagemin: {
+            options: {
+                optimizationLevel: 3,
+                progressive: true,
+                interlaced: true,
+                use: [mozjpeg()]
+            },
+            png: {
+                files: [{
+                    expand: true,
+                    cwd: 'dist/assets/',
+                    src: ['images/png/**/*.png'],
+                    dest: 'dist/assets/'
+                }]
+            },
+            jpg: {
+                files: [{
+                    expand: true,
+                    cwd: 'dist/assets/',
+                    src: ['images/jpg/**/*.jpg'],
+                    dest: 'dist/assets/',
+                }]
+            },
+            gif: {
+                files: [{
+                    expand: true,
+                    cwd: 'dist/assets/',
+                    src: ['images/gif/**/*.gif'],
+                    dest: 'dist/assets/'
+                }]
+            }
+        },
 
-  grunt.registerTask('build',[
-    'uglify',
-    'sass:prod'
-  ]);
+        useminPrepare: {
+            html: '*.html',
+            options: {
+                dest: 'dist',
+                assetsDirs: ['assets/css', 'assets/js', 'assets/images'],
+                patterns: {
+                    js: [
+                        [/(assets\/images\/.*?\.(?:gif|jpeg|jpg|png|webp|svg))/gm, 'Update the JS to reference our revved images']
+                    ],
+                    css: [
+                        [/(?:src=|url\(\s*)['"]?([^'"\)\?#]+)['"]?\s*\)?/gm, 'Update the CSS to reference our revved images']
+                    ]
+                }
+            }
+        },
 
-  grunt.registerTask('regression-test', [
-    'connect',
-    'phantomcss'
-  ]);
+        usemin: {
+            html: ['dist/*.html'],
+            css: ['dist/assets/css/{,*/}*.css'],
+            js: 'dist/assets/js/{,*/}*.js'
+        },
 
-  grunt.registerTask('optimize-images', [
-    'imagemin'
-  ]);
+        filerev: {
+            dist: {
+                files: [{
+                    src: [
+                        'dist/assets/js/{,*/}*.js',
+                        'dist/assets/css/{,*/}*.css',
+                        'dist/assets/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+                        'dist/assets/fonts/(,*/}*.{eot,svg,ttf,woff}'
+                    ]
+                }]
+            }
+        },
 
-  grunt.registerTask('optimize-png', [
-    'imagemin:png'
-  ]);
+        copy: {
+            dist: {
+                files: [
+                    {
+                        src: '*.html',
+                        dest: 'dist/'
+                    },
+                    {
+                        src: 'assets/images/**',
+                        dest: 'dist/',
+                        expand: true
+                    },
+                    {
+                        src: './index.html',
+                        dest: 'dist/index.html'
+                    }
+                ]
+            }
+        },
 
-  grunt.registerTask('optimize-jpg', [
-    'imagemin:jpg'
-  ]);
+        clean: ["dist/", ".tmp/"]
+    });
 
-  grunt.registerTask('optimize-gif', [
-    'imagemin:gif'
-  ]);
+    //TODO: usemin, image optimization,
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-phantomcss');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-filerev');
+    grunt.loadNpmTasks('grunt-usemin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    grunt.registerTask('default', [
+        'connect',
+        'watch'
+    ]);
+
+    grunt.registerTask('build', [
+        'sass:dev',
+        'clean',
+        'copy:dist',
+        'useminPrepare',
+        'concat:generated',
+        'cssmin:generated',
+        'uglify:generated',
+        'filerev',
+        'usemin',
+        'optimize-images'
+    ]);
+
+    grunt.registerTask('regression-test', [
+        'connect',
+        'phantomcss'
+    ]);
+
+    grunt.registerTask('optimize-images', [
+        'imagemin'
+    ]);
+
+    grunt.registerTask('optimize-png', [
+        'imagemin:png'
+    ]);
+
+    grunt.registerTask('optimize-jpg', [
+        'imagemin:jpg'
+    ]);
+
+    grunt.registerTask('optimize-gif', [
+        'imagemin:gif'
+    ]);
 };
